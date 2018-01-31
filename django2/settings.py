f0edd8b13b61c648f5c9081ee3b916d18d03ea28
Haku-Dell <g@kohaku.cc>
2018-01-31 10:55:53 +0800

    set debug to false.

--- django2/settings.py
+++ django2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e@9e9no1owv9)#x3)&__)#!wl&35nkx2efd!mwzdm3$51v@sci'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
