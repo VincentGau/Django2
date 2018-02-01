3d16dfc9c7f54c17bbcf65f962f1ad0988009640
Haku-home <gmkohaku@gmail.com>
2018-02-01 23:46:13 +0800

    basic urls and views

--- django2/urls.py
+++ django2/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hpc/', include('hpc.urls')),
+
+
+    # last path pattern
+    path('', views.home, name='home'),
 ]
