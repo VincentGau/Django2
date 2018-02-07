157ce667c09faf94176ca6be67cb3de33e4726e8
Haku-home <gmkohaku@gmail.com>
2018-02-07 22:17:01 +0800

    raise form errors

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,29 @@
 from django import forms
+from django.contrib.auth import authenticate
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length='100')
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(max_length='100',
+                               error_messages={'required': 'username is required.'},
+                               widget=forms.TextInput(
+                                   attrs={
+                                       'placeholder': 'username'
+                                   }
+                               )
+                               )
+    password = forms.CharField(error_messages={'required': 'password is required'},
+                               widget=forms.PasswordInput(
+                                   attrs={
+                                       'placeholder': 'password'
+                                   }
+                               ))
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        if not user:
+            raise forms.ValidationError('Login failed, please try again')
+        if not user.is_active:
+            raise forms.ValidationError('User is not active, please contact administrator.')
+        return self.cleaned_data
--- account/views.py
+++ account/views.py
@@ -1,5 +1,5 @@
 from django.contrib import auth
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from .forms import LoginForm
@@ -10,7 +10,7 @@ from .forms import LoginForm
 def login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
-        if form.is_valid():
+        if form.is_valid():  # is_valid() call the clean() method in LoginForm
             cd = form.cleaned_data
             user = auth.authenticate(username=cd['username'],
                                      password=cd['password'])
@@ -18,10 +18,16 @@ def login(request):
                 auth.login(request, user)
                 return redirect('/')
             else:
-                return HttpResponseRedirect(reverse('account:login'))
+                return render(request, 'account/login.html', {'form': form})
         else:
-            return redirect('/')
+            return render(request, 'account/login.html', {'form': form})
 
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect(reverse("account:login"))
+
