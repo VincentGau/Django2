d9369845d9c8400b7d1c052cabaa28447cfc24c2
Haku-Dell <g@kohaku.cc>
2018-02-08 08:23:10 +0800

    [trigger test]

--- hpc/tests.py
+++ hpc/tests.py
@@ -8,11 +8,11 @@ class HomePageViewTest(TestCase):
     def test_home_page(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Home Page.')
+        self.assertContains(response, 'index')
 
 
 class HpcViewsTest(TestCase):
     def test_hpc_home_page(self):
         response = self.client.get(reverse('hpc:index'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'hpc main page.')
+        self.assertContains(response, 'login')
