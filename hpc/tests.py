e4a56f08ba7d80757067e9ee6a940f86b31b9753
Haku-home <gmkohaku@gmail.com>
2018-02-10 21:47:01 +0800

    fix test cases

--- hpc/tests.py
+++ hpc/tests.py
@@ -1,18 +1,41 @@
-from django.test import TestCase
+
+from django.test import TestCase, Client
 
 # Create your tests here.
 from django.urls import reverse
+
+from hpc.models import User
 
 
 class HomePageViewTest(TestCase):
     def test_home_page(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Home Page.')
+        self.assertContains(response, 'Index')
+
+    def test_authenticated_page(self):
+        self.client.force_login(User.objects.get_or_create(username='testuser')[0])
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        print(response.content)
+        self.assertContains(response, 'testuser')
+
+    def test_not_authenticated_page(self):
+        self.client.logout()
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'login')
 
 
 class HpcViewsTest(TestCase):
     def test_hpc_home_page(self):
         response = self.client.get(reverse('hpc:index'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'hpc main page.')
+        self.assertContains(response, 'Index')
+
+
+class AccountViewsTest(TestCase):
+    def test_login_page(self):
+        response = self.client.get(reverse('account:login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'login')
--- hpc/views.py
+++ hpc/views.py
@@ -6,11 +6,3 @@ from django.shortcuts import render, render_to_response
 
 def index(request):
     return render(request, 'hpc/index.html')
-
-
-def page_not_found():
-    return render('hpc/404.html')
-
-
-def server_error():
-    return render('hpc/404.html')
