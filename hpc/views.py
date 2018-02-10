41bfdafde7d2a8d5163edaa7a8ef5260cb5ed129
Haku-home <gmkohaku@gmail.com>
2018-02-10 08:02:41 +0800

    use render instead of render_to_response

--- hpc/views.py
+++ hpc/views.py
@@ -5,12 +5,12 @@ from django.shortcuts import render, render_to_response
 # Create your views here.
 
 def index(request):
-    return render_to_response('hpc/index.html')
+    return render(request, 'hpc/index.html')
 
 
 def page_not_found():
-    return render_to_response('hpc/404.html')
+    return render('hpc/404.html')
 
 
 def server_error():
-    return render_to_response('hpc/404.html')
+    return render('hpc/404.html')
