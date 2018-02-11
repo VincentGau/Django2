2f8a37a5c3fd81f250382686973ed323f95ab77a
Haku-Dell <g@kohaku.cc>
2018-02-11 17:32:32 +0800

    static root

--- django2/settings.py
+++ django2/settings.py
@@ -16,6 +16,10 @@ import local_settings
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# production web server will handler the static files when DEBUG is set to false, places to serve these files.
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -23,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # TODO set allowed hosts
 ALLOWED_HOSTS = ['new.kohaku.cc', 'localhost', '127.0.0.1']
--- hpc/tests.py
+++ hpc/tests.py
@@ -1,10 +1,16 @@
-
-from django.test import TestCase, Client
+from django.test import TestCase, override_settings
+from django.contrib.staticfiles import finders
 
 # Create your tests here.
 from django.urls import reverse
 
 from hpc.models import User
+
+
+class StaticFileTest(TestCase):
+    def test_css_file(self):
+        result = finders.find('hpc/css/style.css')
+        self.assertNotEqual(result, None)
 
 
 class HomePageViewTest(TestCase):
@@ -17,7 +23,6 @@ class HomePageViewTest(TestCase):
         self.client.force_login(User.objects.get_or_create(username='testuser')[0])
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        print(response.content)
         self.assertContains(response, 'testuser')
 
     def test_not_authenticated_page(self):
@@ -39,3 +44,15 @@ class AccountViewsTest(TestCase):
         response = self.client.get(reverse('account:login'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'login')
+
+
+class MultiDomainTest(TestCase):
+    @override_settings(ALLOWED_HOSTS=['www.test.com'])
+    def test_other_domain(self):
+        response = self.client.get('/', HTTP_HOST='kohaku.cc')
+        self.assertEqual(response.status_code, 400)
+
+    @override_settings(ALLOWED_HOSTS=['kohaku.cc'])
+    def test_correct_domain(self):
+        response = self.client.get('/', HTTP_HOST='kohaku.cc')
+        self.assertEqual(response.status_code, 200)
